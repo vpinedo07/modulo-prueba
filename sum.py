98497074e9b6e41abba93202210db5889b63845b
vpinedo07 <victor.pf@pachuca.tecnm.mx>
2024-07-30 16:28:15 -0600

    Códigos del módulo 1

---
+++ sum.py
@@ -0,0 +1,14 @@
+# ********************
+# LA SUMA MÁS SENCILLA
+# ********************
+
+
+def run(a: int, b: int) -> int:
+    # TU CÓDIGO AQUÍ
+    result = a+b
+
+    return result
+
+
+if __name__ == '__main__':
+    run(3, 4)
